5def175831323e8550b8dba1c150dcf297a4c65f
stormhouse <stormhouse@yeah.net>
2015-03-13 15:15:19 +0800

    add Snap

--- UIWithoutStoryboard/RootViewController.swift
+++ UIWithoutStoryboard/RootViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Snap
 
 class RootViewController: UIViewController {
 
@@ -16,73 +17,21 @@ class RootViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let superview = self.view
-        
-        innerView = UIView()
-        innerView.setTranslatesAutoresizingMaskIntoConstraints(false)
-        innerView.backgroundColor = UIColor.darkGrayColor()
-        superview.addSubview(innerView)
-        
-        let padding = UIEdgeInsetsMake(10, 10, 10, 10)
-        
-        superview.addConstraints([
-            NSLayoutConstraint(
-                item: innerView,
-                attribute: NSLayoutAttribute.Top,
-                relatedBy: NSLayoutRelation.Equal,
-                toItem: superview,
-                attribute: NSLayoutAttribute.Top,
-                multiplier: 1.0,
-                constant: padding.top
-            ),
-            NSLayoutConstraint(
-                item: innerView,
-                attribute: NSLayoutAttribute.Left,
-                relatedBy: NSLayoutRelation.Equal,
-                toItem: superview,
-                attribute: NSLayoutAttribute.Left,
-                multiplier: 1.0,
-                constant: padding.left
-            ),
-            NSLayoutConstraint(
-                item: innerView,
-                attribute: NSLayoutAttribute.Bottom,
-                relatedBy: NSLayoutRelation.Equal,
-                toItem: superview,
-                attribute: NSLayoutAttribute.Bottom,
-                multiplier: 1.0,
-                constant: -padding.bottom
-            ),
-            NSLayoutConstraint(
-                item: innerView,
-                attribute: NSLayoutAttribute.Right,
-                relatedBy: NSLayoutRelation.Equal,
-                toItem: superview,
-                attribute: NSLayoutAttribute.Right,
-                multiplier: 1.0,
-                constant: -padding.right
-            )
-            ])
-        
         button = UIButton.buttonWithType(.System) as? UIButton
-        innerView.addSubview(button)
+        self.view.addSubview(button)
         
         button.backgroundColor = UIColor.greenColor()
-//        button.frame = CGRect(x: 110, y: 70, width: 100, height: 44)
-
-        button.snp_makeConstraints { make in
-            make.left.right.and.bottom.equalTo(superview)
-            make.top.equalTo(otherView)
-            return // this return is a fix for implicit returns in Swift and is only required for single line constraints
-        }
         button.setTitle("Press Me", forState: .Normal)
         button.setTitle("I'm Pressed", forState: .Highlighted)
         button.addTarget(self, action: "buttonIsPressed:", forControlEvents: .TouchDown)
         button.addTarget(self, action: "buttonIsTapped:", forControlEvents: .TouchUpInside)
+//        button.frame = CGRect(x: 110, y: 70, width: 100, height: 44)
 
+        button.snp_makeConstraints { make in
+            make.center.equalTo(self.view).offset(CGPointMake(-5, 10))
+            return // this return is a fix for implicit returns in Swift and is only required for single line constraints
+        }
 
-
-        // Do any additional setup after loading the view.
     }
     
     func buttonIsPressed(sender: UIButton){ println("Button is pressed.")}
@@ -91,7 +40,6 @@ class RootViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
